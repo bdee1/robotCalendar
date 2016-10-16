34fac90ed01dcd1c2c3b931aa46d2b308cb8cd79
bdee1 <bdee@deewebdesign.com>
2016-10-16 19:00:53 -0400

    fixed the slider on the colorpicker
    The slider on the color picker didn’t let you go all the way down.

--- public/js/script.js
+++ public/js/script.js
@@ -28,8 +28,8 @@ $(document).ready(function() {
 	        }
 	    });
     }
-    
-    
+
+
     if (jQuery().datetimepicker) {
 	    //initialize the datepickers
 	    $('.datetimepicker').datetimepicker({
@@ -40,29 +40,41 @@ $(document).ready(function() {
 		    }
 	    });
 	}
-	
+
 	if (jQuery().colorpicker) {
 		//initialize the colorpickers
 		$('.colorpicker').colorpicker({
 			format: 'hex',
-			customClass: 'colorpicker-2x'
-		})
+			customClass: 'colorpicker-2x',
+      sliders: {
+          saturation: {
+              maxLeft: 200,
+              maxTop: 200
+          },
+          hue: {
+              maxTop: 200
+          },
+          alpha: {
+              maxTop: 200
+          }
+      }
+		});
 	}
-	
+
 	//bulk approve ajax function for manage requests page
 	$('.bulk_approved').change(function() {
 		var approved = 0;
 		var robotID = 0;
-		
+
 		if ( $(this).is(":checked") ) {
 			approved = 1;
 			//alert ('approved');
 		}
-		
+
 		robotID = $(this).attr("id");
 		robotID = robotID.split('_')[1];
-		
-		
+
+
 		$.ajax({
 			url: "bulk_approve.php",
 			data: {
@@ -72,7 +84,7 @@ $(document).ready(function() {
 			type: "POST"
 		})
 		.done(function() {
-			//alert('request complete');	
+			//alert('request complete');
 		});
 	});
 });
